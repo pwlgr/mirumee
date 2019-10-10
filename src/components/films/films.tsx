a9cc0e00095a6647f4ad8a8e45bbd26db93c680e
Paweł Waligóra <pawelwaligora1337@gmail.com>
2019-10-10 19:44:58 +0200

    add local storage persistence

--- src/components/films/films.tsx
+++ src/components/films/films.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect, useState, useReducer } from 'react';
+import { useEffect, useState } from 'react';
 import uuid from 'uuid/v1';
 import Film from '../film/film';
 import NewFilm from '../newFilm/newFilm';
@@ -10,17 +10,20 @@ import './scss/index.scss';
 const Films: React.FC = () => {
 	const [ loading, setLoading ] = useState(false);
 	const [ films, setFilms ] = useState([]);
+	const localFilms = JSON.parse(localStorage.getItem('films')) || [];
 	useEffect(() => {
 		const getFilms = async () => {
 			setLoading(true);
 			const result = await fetch('https://swapi.co/api/films/');
 			const data = await result.json();
 			setLoading(false);
-			setFilms(data.results.map((e) => ({ title: e.title, planets: e.planets })));
+			setFilms([ ...data.results.map((e) => ({ title: e.title, planets: e.planets })), ...localFilms ]);
 		};
 		getFilms();
 	}, []);
 	const addNewFilm = (film) => {
+		const updatedLocalFilms = [ ...localFilms, film ];
+		localStorage.setItem('films', JSON.stringify(updatedLocalFilms));
 		setFilms([ ...films, film ]);
 	};
 	return (
